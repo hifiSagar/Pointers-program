ff3834eb6b88d9e0803cceab6922491740f23f84
SAGAR T <ssagu6661@gmail.com>
2024-10-16 16:45:54 +0530

    Add files via upload

---
+++ basic_program35.c
@@ -0,0 +1,31 @@
+// pointers program
+
+
+// #include<stdio.h>
+// void main(){
+//     // int a=10;
+//     // float b=10.0202;
+//     // int*ptr1=&a;
+//     // int*ptr2=&b;
+//     // printf("Value of a is : %d\nValue of B is : %d\n",a,b);
+//     float x,y;
+//     int a, *ptr;
+//     ptr=&x;
+//     ptr=&a;
+//     printf("%d\n",x);
+//     printf("%d\n",a);
+// }
+
+
+#include<stdio.h>
+void main(){
+int a=10,b=9;
+int *p,*q;
+p=&a;
+q=&b;
+// *q=*b;
+printf("%d\n",*p);
+printf("%d\n",q);
+// printf("%d\n",*q);
+
+}
